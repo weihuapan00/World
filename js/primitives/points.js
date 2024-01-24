bde574460d872768963e5895b2022d97a83388ad
william <w5pan@ucsd.edu>
2024-01-23 21:32:08 -0800

    redesign user interface

--- js/primitives/points.js
+++ js/primitives/points.js
@@ -8,11 +8,26 @@ class Point{
         return this.x == p.x && this.y == p.y;
     }
     
-    draw(ctx,size=18,color ="black"){
+    draw(ctx, {size=18,color ="black", outline = false, fill=false} = {}){
         const rad = size/2;
         ctx.beginPath();
         ctx.fillStyle = color;
         ctx.arc(this.x,this.y,rad,0,Math.PI * 2);
         ctx.fill()
+        // draw differently if the point is selected
+        if (outline) {
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "White";
+            ctx.arc(this.x,this.y,rad*0.6,0,Math.PI * 2);
+            ctx.stroke();
+        }
+        // draw differently if the point is hovered
+        if (fill){
+            ctx.beginPath();
+            ctx.fillStyle = "Yellow";
+            ctx.arc(this.x,this.y,rad*0.4,0,Math.PI * 2);
+            ctx.fill();
+        }
     }
 }
